0390c20ebc9f98c38ac04e32370c39a7b0e0bec3
Samantha Long <sam43092@ymail.com>
2018-10-04 03:32:46 +0000

    Update to body parsing

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express'),
 app = express(),
 port = process.env.PORT || 8080;
-
+const bodyParser  = require('body-parser');
+app.use(bodyParser.json());
 const routes = require('./api/routes/routes'); //importing routes
 routes(app);
 
