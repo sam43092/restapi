2058fedc1c5d185f00a450ae7f5e43364ab53c39
Samantha Long <sam43092@ymail.com>
2018-10-04 03:31:08 +0000

    Updating body parsing

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,17 +1,11 @@
 //Implementation of defined routes. For now, simple
 //JSON messages are returned
 
-const bodyParser  = require('body-parser');
-'use strict';
+
 
 //Create a new review on the database
 exports.create_a_review = function(req, res) {
-   res.json({message: 'Create New Review'});
-    
-    //I looked up what to do with the body parsing but this will not send
-    //the body to the server on Postman
-    const new_review = bodyParser.json(req.body);
-    res.json(new_review);
+    console.log(req.body);
 };
 
 //Get a review from the database
@@ -21,9 +15,7 @@ exports.read_a_review = function(req, res) {
 
 //Update a review in the database
 exports.update_a_review = function(req, res) {
-    res.json({message: 'Updating Review'});
-    const new_review = bodyParser.json(req.body);
-    res.json(new_review);
+    console.log(req.body);
 };
 
 //Delete a review from the database
