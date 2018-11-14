2830356070d41a92afedbe4ab6fa4290630adb2a
Samantha Long <sam43092@ymail.com>
2018-11-14 04:55:31 +0000

    new server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 //Creates server
+var MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://omega.unasec.info";
 
-const express = require('express'),
-app = express(),
-port = process.env.PORT || 8080;
-const bodyParser  = require('body-parser');
-app.use(bodyParser.json());
-const routes = require('./api/routes/routes'); //importing routes
-routes(app);
-
-app.listen(port);
-
-//Check to see what port is being used
-console.log('RESTful API server started on: ' + port);
+MongoClient.connect(url, function(err, client){
+    if (err) throw err;
+    const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080;
+    const bodyParser  = require('body-parser');
+    app.use(bodyParser.json());
+    const routes = require('./api/routes/routes'); //importing routes
+    routes(app);
+    
+    app.listen(port);
+    
+    //Check to see what port is being used
+    console.log('RESTful API server started on: ' + port);
+});
